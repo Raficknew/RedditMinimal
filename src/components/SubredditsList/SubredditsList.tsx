2852009c94a30615c9540ad592a882787e282f61
Raficknew <rafi20302@gmail.com>
2026-01-10 17:36:33 +0100

    added list of subreddits

--- src/components/SubredditsList/SubredditsList.tsx
+++ src/components/SubredditsList/SubredditsList.tsx
@@ -1,7 +1,47 @@
-export function SubredditsList() {
+import type { Subreddit } from "./subreddits";
+
+export function SubredditsList({
+  subreddits,
+  selectedSubreddit,
+  onSubredditClick,
+}: {
+  subreddits: Subreddit[];
+  selectedSubreddit: string | null;
+  onSubredditClick: (endpoint: string) => void;
+}) {
   return (
-    <aside className="flex sm:w-87.5 p-4 bg-white shadow-lg">
+    <aside className="flex flex-col w-full md:w-125 p-4 bg-white shadow-lg">
       <h2 className="text-2xl text-[#444444] font-semibold">Subreddits</h2>
+      {subreddits.map((subreddit) => (
+        <Subreddit
+          key={subreddit.name}
+          selected={selectedSubreddit === subreddit.endpoint}
+          onClick={() => onSubredditClick(subreddit.endpoint)}
+          subreddit={subreddit}
+        />
+      ))}
     </aside>
+  );
+}
+
+function Subreddit({
+  subreddit,
+  selected,
+  onClick,
+}: {
+  subreddit: Subreddit;
+  selected: boolean;
+  onClick: () => void;
+}) {
+  return (
+    <div
+      onClick={onClick}
+      className={`flex items-center cursor-pointer h-13 ${
+        selected &&
+        "bg-indigo-400 border-l-5 text-indigo-700 font-semibold border-indigo-700 pl-3"
+      }`}
+    >
+      {subreddit.name}
+    </div>
   );
 }
---
+++ src/components/SubredditsList/subreddits.ts
@@ -0,0 +1,135 @@
+export type Subreddit = {
+  name: string;
+  endpoint: string;
+  icon: string;
+};
+
+export const SUBREDDITS = {
+  categories: [
+    {
+      name: "funny",
+      endpoint: "r/funny",
+      icon: "",
+    },
+    {
+      name: "askreddit",
+      endpoint: "r/AskReddit",
+      icon: "",
+    },
+    {
+      name: "gaming",
+      endpoint: "r/gaming",
+      icon: "",
+    },
+    {
+      name: "worldnews",
+      endpoint: "r/worldnews",
+      icon: "",
+    },
+    {
+      name: "todayilearned",
+      endpoint: "r/todayilearned",
+      icon: "",
+    },
+    {
+      name: "music",
+      endpoint: "r/Music",
+      icon: "",
+    },
+    {
+      name: "aww",
+      endpoint: "r/aww",
+      icon: "",
+    },
+    {
+      name: "movies",
+      endpoint: "r/movies",
+      icon: "",
+    },
+    {
+      name: "memes",
+      endpoint: "r/memes",
+      icon: "",
+    },
+    {
+      name: "science",
+      endpoint: "r/science",
+      icon: "",
+    },
+    {
+      name: "showerthoughts",
+      endpoint: "r/Showerthoughts",
+      icon: "",
+    },
+    {
+      name: "pics",
+      endpoint: "r/pics",
+      icon: "",
+    },
+    {
+      name: "news",
+      endpoint: "r/news",
+      icon: "",
+    },
+    {
+      name: "jokes",
+      endpoint: "r/Jokes",
+      icon: "",
+    },
+    {
+      name: "space",
+      endpoint: "r/space",
+      icon: "",
+    },
+    {
+      name: "diy",
+      endpoint: "r/DIY",
+      icon: "",
+    },
+    {
+      name: "books",
+      endpoint: "r/books",
+      icon: "",
+    },
+    {
+      name: "videos",
+      endpoint: "r/videos",
+      icon: "",
+    },
+    {
+      name: "askscience",
+      endpoint: "r/askscience",
+      icon: "",
+    },
+    {
+      name: "nottheonion",
+      endpoint: "r/nottheonion",
+      icon: "",
+    },
+    {
+      name: "mildlyinteresting",
+      endpoint: "r/mildlyinteresting",
+      icon: "",
+    },
+    {
+      name: "food",
+      endpoint: "r/food",
+      icon: "",
+    },
+    {
+      name: "getmotivated",
+      endpoint: "r/GetMotivated",
+      icon: "",
+    },
+    {
+      name: "art",
+      endpoint: "r/Art",
+      icon: "",
+    },
+    {
+      name: "explainlikeimfive",
+      endpoint: "r/explainlikeimfive",
+      icon: "",
+    },
+  ],
+};
